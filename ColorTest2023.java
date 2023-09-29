6a4f8beb998f2bec2b443349beb096ee97130eac
JordanVL1234 <96630742+JordanVL1234@users.noreply.github.com>
2023-09-29 15:06:11 -0400

    Updated ColorTest2023.java
    It "works" with updated controls.

--- ColorTest2023.java
+++ ColorTest2023.java
@@ -1,36 +1,56 @@
-   * This function is executed when this Op Mode is selected from the Driver Station.
-   */
-  @Override
-  public void runOpMode() {
-    BackL = hardwareMap.get(DcMotor.class, "BackL");
-    BackR = hardwareMap.get(DcMotor.class, "BackR");
-    FrontL = hardwareMap.get(DcMotor.class, "FrontL");
-    FrontR = hardwareMap.get(DcMotor.class, "FrontR");
-    colorsensor_LightSensor = hardwareMap.get(LightSensor.class, "colorsensor");
-    colorsensor = hardwareMap.get(ColorSensor.class, "colorsensor");
+package org.firstinspires.ftc.teamcode;
 
-    // Put initialization blocks here.
-    waitForStart();
-    if (opModeIsActive()) {
-      // Put run blocks here.
-      BackL.setPower(-0.2);
-      BackR.setPower(0.2);
-      FrontL.setPower(-0.2);
-      FrontR.setPower(0.2);
-      while (opModeIsActive()) {
-        // Put loop blocks here.
-        telemetry.addData("Light Detected", colorsensor_LightSensor.getLightDetected());
-        telemetry.addData("Red", colorsensor.red());
-        telemetry.addData("Blue", colorsensor.blue());
-        telemetry.addData("Green", colorsensor.green());
-        telemetry.update();
-        if (colorsensor.red() > 150) {
-          BackL.setPower(0);
-          BackR.setPower(0);
-          FrontL.setPower(0);
-          FrontR.setPower(0);
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@TeleOp(name = "ColorTest2023 (Blocks to Java)")
+public class ColorTest2023BTJ extends LinearOpMode {
+    // Define your class members here
+    DcMotor BackL, BackR, FrontL, FrontR;
+    LightSensor colorsensor_LightSensor;
+    ColorSensor colorsensor;
+
+    @Override
+    public void runOpMode() {
+        double OgMotorSpeed = 0.2;
+        double DyMotorSpeed;
+
+        BackL = hardwareMap.get(DcMotor.class, "BackL");
+        BackR = hardwareMap.get(DcMotor.class, "BackR");
+        FrontL = hardwareMap.get(DcMotor.class, "FrontL");
+        FrontR = hardwareMap.get(DcMotor.class, "FrontR");
+        colorsensor_LightSensor = hardwareMap.get(LightSensor.class, "colorsensor");
+        colorsensor = hardwareMap.get(ColorSensor.class, "colorsensor");
+
+        waitForStart();
+        if (opModeIsActive()) {
+            BackL.setPower(OgMotorSpeed * -1);
+            BackR.setPower(OgMotorSpeed);
+            FrontL.setPower(OgMotorSpeed * -1);
+            FrontR.setPower(OgMotorSpeed);
+            while (opModeIsActive()) {
+                telemetry.addData("Light Detected", colorsensor_LightSensor.getLightDetected());
+                telemetry.addData("Red", colorsensor.red());
+                telemetry.addData("Blue", colorsensor.blue());
+                telemetry.addData("Green", colorsensor.green());
+                telemetry.update();
+                int SeenColor = 0;
+                if (colorsensor.red() > (colorsensor.blue()+colorsensor.green())/1.5 && SeenColor == 0) {
+                    BackL.setPower(OgMotorSpeed);
+                    BackR.setPower(OgMotorSpeed * -1);
+                    FrontL.setPower(OgMotorSpeed);
+                    FrontR.setPower(OgMotorSpeed * -1);
+                    BackL.setPower(0);
+                    BackR.setPower(0);
+                    FrontL.setPower(0);
+                    FrontR.setPower(0);
+                    SeenColor = 1;
+                }
+            }
         }
-      }
     }
-  }
 }
