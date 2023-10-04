de0627a3a834125f0cdbaa86a7b92e4502ff0682
Alyssa Young <145914904+RainyAlly@users.noreply.github.com>
2023-10-04 18:06:59 -0400

    Create codeSkeleton.java

---
+++ codeSkeleton.java
@@ -0,0 +1,23 @@
+ waitForStart();
+  // (Move desired distance);
+  // (Scan for cone);
+  if (colorsensor.red() > (colorsensor.blue()+colorsensor.green())/1.5 && SeenColor == 0) {
+    // (Drop pixel);
+    // (Turn 90 degrees left);
+    System.exit(1)
+  } else {
+    // (Turn 90 degrees left);
+    // (Scan for cone);
+  } if (colorsensor.red() > (colorsensor.blue()+colorsensor.green())/1.5 && SeenColor == 0) {
+    // (Drop pixel);
+    // (Turn 180 degrees left);
+    System.exit(1)
+  } else {
+    // (Turn 180 degrees left);
+    // (Scan for cone);
+  } if (colorsensor.red() > (colorsensor.blue()+colorsensor.green())/1.5 && SeenColor == 0) {
+    (Drop pixel);
+    System.exit(1)
+      }
+  }
+}
