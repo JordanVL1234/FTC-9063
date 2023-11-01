2097880335ee595abdb05166077f791e5386e2d8
Preston <147774693+Las0rKitten@users.noreply.github.com>
2023-11-01 18:59:40 -0400

    Update Test2.java

--- P.S./Test2.java
+++ P.S./Test2.java
@@ -1,1 +1,34 @@
+package org.firstinspires.ftc.teamcode:
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.robotcore.external.State;
+import java.util.Set;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name= "Test2")
+  public class Test2 extends LinearOpMode {
+
+public enum DriveState {
+  Stop,
+  Forward,
+  Backward,
+  CW,
+  CCW,
+  Left,
+  Right
+}
+
+enum TestForColor {
+  Red,
+  Green,
+  Blue
+}
+    public DcMotor FrontR;
+    public DcMotor FrontL;
+    public DcMotor BackR;
+    public DcMotor BackL;
+    public DcMotor pickupMotor
